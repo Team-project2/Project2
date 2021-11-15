b68f33335da7142ce8501da14e16e9e7761b3a12
mahaManae <johndoe@example.com>
2021-11-15 16:52:06 +0300

    git commit -m"project"

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,32 +1,38 @@
 import React, { useState ,useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import axios, { Axios } from 'axios';
-
 export default function Book() {
     const p = useParams();
     const [bookList, setBooksList] = useState([])
-
     useEffect(async() => {
-        const result = await axios.get(`http://localhost:3000/book/${p.id}`);
+        const result = await axios.get(`http://localhost:5000/book/${p.id}`);
         console.log(result.data,"result.data")
         setBooksList(result.data);
      },[])
-    
+    // const videoId = "watch?v=vm_kACNrF3E"
     return (
-     
-      <div className="booksImg"> 
-      <div className="bookcontainer">
-          <h1>{bookList[0] && bookList[0].name}</h1>
-          <img className="bookimg" src={bookList[0] && bookList[0].imgUrl}/>
-                  <h4>Writer: {bookList[0] && bookList[0].writer}</h4>
-                  <h3>Discription :</h3>
-                  <p>{bookList[0] && bookList[0].discreption}</p>
-      
-        {/* <video width="320" height="240" controls>
-          <source src="movie.mp4" type="video/mp4">
-          <source src="movie.ogg" type="video/ogg">
-        </video> */}
-        </div>
-    </div>
+     bookList.length ?  <div >
+     <div className="bookcontainer">
+         <h1>{bookList[0] && bookList[0].name}</h1>
+         <img className="bookimg" src={bookList[0] && bookList[0].imgUrl}/>
+         <hr/>
+         <br/>
+                 {/* <h4>Writer: {bookList[0] && bookList[0].writer}</h4> */}
+                 <div className="descrrr">
+                 <h3>Discription :</h3>
+                 <p>{bookList[0] && bookList[0].discreption}</p>
+                 <br/></div>
+                 <h3>Triller :</h3>
+                 <iframe
+       width="420"
+       height="315"
+       src={`https://www.youtube.com/embed/${bookList[0].videoId}`}
+     ></iframe>
+       {/* <video width="320" height="240" controls>
+         <source src="movie.mp4" type="video/mp4">
+         <source src="movie.ogg" type="video/ogg">
+       </video> */}
+       </div>
+   </div>:""
     );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
+// export default function Home() {
+//     return (
+//         <div className="body">
+//         <div className="bk">
+
+//           <h1 className="Hometitle">  📕 <br/> Think before you speak,<br/> Read before you think ..</h1>
+
+//         </div>
+//         <img src="https://s3-eu-west-1.amazonaws.com/tra-src/uploads/asset/file/345/WWH_Page_Breaker_No_1.png"/>
+//         {/* <video>
+//    <source src="https://www.youtube.com/watch?v=vm_kACNrF3E"></source>
+//  </video> */}
+
+//         </div>
+//     )
+// }
+import React from 'react'
 export default function Home() {
     return (
-        <div className="body">
+        <div className="bodyHome">
         <div className="bk">
-
-          <h1 className="Hometitle">  📕 <br/> Think before you speak,<br/> Read before you think ..</h1>
-
         </div>
-        <img src="https://s3-eu-west-1.amazonaws.com/tra-src/uploads/asset/file/345/WWH_Page_Breaker_No_1.png"/>
-        {/* <video>
-   <source src="https://www.youtube.com/watch?v=vm_kACNrF3E"></source>
- </video> */}
-
+        <h1 className="Hometitle">  <br/> WHAT TO WATCH ? <br/> 🎈 KIDS MOVIES 🎈 </h1>
         </div>
     )
 }
